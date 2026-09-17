5f7f4e7190646de233b4343c463162c257a690af
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-17 23:29:51 +0100

    feat(app): inject persistent drive history into HUD

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'features/hud/hud_screen.dart';
 import 'features/settings/app_settings.dart';
+import 'features/driving/drive_history.dart';
 
 class SixVitessesApp extends StatelessWidget {
-  const SixVitessesApp({super.key, required this.settings});
+  const SixVitessesApp({super.key, required this.settings, required this.history});
   final AppSettings settings;
+  final DriveHistory history;
 
   @override
   Widget build(BuildContext context) => AnimatedBuilder(
@@ -13,7 +15,7 @@ class SixVitessesApp extends StatelessWidget {
       title: '6 Vitesses',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(useMaterial3: true),
-      home: HudScreen(settings: settings),
+      home: HudScreen(settings: settings, history: history),
     ),
   );
 }
