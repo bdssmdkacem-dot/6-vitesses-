c56ec27e7eca0541037aa7ffb108aded389f46fe
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-17 23:28:14 +0100

    feat: load persistent application settings

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'features/hud/hud_screen.dart';
+import 'features/settings/app_settings.dart';
 
 class SixVitessesApp extends StatelessWidget {
-  const SixVitessesApp({super.key});
+  const SixVitessesApp({super.key, required this.settings});
+  final AppSettings settings;
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-    title: '6 Vitesses',
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData.dark(useMaterial3: true),
-    home: const HudScreen(),
+  Widget build(BuildContext context) => AnimatedBuilder(
+    animation: settings,
+    builder: (_, __) => MaterialApp(
+      title: '6 Vitesses',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(useMaterial3: true),
+      home: HudScreen(settings: settings),
+    ),
   );
 }
