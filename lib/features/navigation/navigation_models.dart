062bfe8974f3fade71c612719d806abd7dbd4be0
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-18 22:54:52 +0100

    feat(navigation): add maneuver route context

--- lib/features/navigation/navigation_models.dart
+++ lib/features/navigation/navigation_models.dart
@@ -3,13 +3,31 @@ import 'package:latlong2/latlong.dart';
 enum NavigationManeuverType { depart, arrive, turnLeft, turnRight, sharpLeft, sharpRight, uTurn, straight, roundabout, merge, fork, offRamp, onRamp, endOfRoad, unknown }
 
 class NavigationManeuver {
-  const NavigationManeuver({required this.type, required this.position, required this.distanceMeters, this.name, this.modifier, this.exitNumber});
+  const NavigationManeuver({
+    required this.type,
+    required this.position,
+    required this.distanceMeters,
+    this.name,
+    this.modifier,
+    this.exitNumber,
+    this.bearingBefore,
+    this.bearingAfter,
+    this.roadRef,
+    this.routeProgressMeters,
+    this.routeSegmentIndex,
+  });
+
   final NavigationManeuverType type;
   final LatLng position;
   final double distanceMeters;
   final String? name;
   final String? modifier;
   final int? exitNumber;
+  final double? bearingBefore;
+  final double? bearingAfter;
+  final String? roadRef;
+  final double? routeProgressMeters;
+  final int? routeSegmentIndex;
 }
 
 class NavigationState {
@@ -25,7 +43,9 @@ class NavigationState {
     this.nextManeuverDistanceMeters = 0,
     this.routeProgressMeters = 0,
     this.routeTotalMeters = 0,
+    this.routeSegmentIndex = 0,
   });
+
   final List<LatLng> route;
   final List<NavigationManeuver> maneuvers;
   final int nextIndex;
@@ -37,6 +57,12 @@ class NavigationState {
   final double nextManeuverDistanceMeters;
   final double routeProgressMeters;
   final double routeTotalMeters;
-  NavigationManeuver? get nextManeuver => nextIndex >= 0 && nextIndex < maneuvers.length ? maneuvers[nextIndex] : null;
+  final int routeSegmentIndex;
+
+  NavigationManeuver? get nextManeuver =>
+      nextIndex >= 0 && nextIndex < maneuvers.length
+          ? maneuvers[nextIndex]
+          : null;
+
   bool get arrived => nextManeuver?.type == NavigationManeuverType.arrive;
 }
