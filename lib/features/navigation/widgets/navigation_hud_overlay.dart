49078fd88eb478367f80cac7cb77fdc346611d9d
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-19 00:24:41 +0100

    fix(hud): repair navigation overlay widget tree syntax

--- lib/features/navigation/widgets/navigation_hud_overlay.dart
+++ lib/features/navigation/widgets/navigation_hud_overlay.dart
@@ -53,8 +53,6 @@ class NavigationHudOverlay extends StatelessWidget {
             ),
           ),
         ),
-      ),
-    );
   }
 
   String _distance(double meters) => meters < 1000 ? '${meters.round()} m' : '${(meters / 1000).toStringAsFixed(1)} km';
