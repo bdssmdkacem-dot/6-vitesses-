e35c499c30676ba3c4e1d5bc0b07ebd769c395da
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-19 01:15:33 +0100

    feat(settings): persist Digital GT layout selection

--- lib/features/settings/app_settings.dart
+++ lib/features/settings/app_settings.dart
@@ -7,10 +7,11 @@ enum SpeedUnit { kmh, mph }
 class AppSettings extends ChangeNotifier {
   AppSettings._(this._prefs);
   final SharedPreferences _prefs;
-  static const _themeKey='hud_theme', _gaugeKey='hud_gauge', _mirrorKey='hud_mirror', _gearKey='hud_gear', _unitKey='speed_unit', _limitKey='speed_limit', _vehicleNameKey='vehicle_name', _vehicleModelKey='vehicle_model', _obdEnabledKey='obd_enabled', _animationsKey='hud_animations', _rpmKey='hud_rpm', _compactKey='hud_compact';
+  static const _themeKey='hud_theme', _gaugeKey='hud_gauge', _gtLayoutKey='gt_layout', _mirrorKey='hud_mirror', _gearKey='hud_gear', _unitKey='speed_unit', _limitKey='speed_limit', _vehicleNameKey='vehicle_name', _vehicleModelKey='vehicle_model', _obdEnabledKey='obd_enabled', _animationsKey='hud_animations', _rpmKey='hud_rpm', _compactKey='hud_compact';
 
   HudTheme _theme=HudTheme.midnight;
   HudGaugeStyle _gauge=HudGaugeStyle.digital;
+  GtLayout _gtLayout=GtLayout.nav;
   bool _mirror=false,_obdEnabled=false,_animations=true,_showRpm=true,_compact=false;
   int _gear=0;
   SpeedUnit _unit=SpeedUnit.kmh;
@@ -22,6 +23,7 @@ class AppSettings extends ChangeNotifier {
     final s=AppSettings._(prefs);
     s._theme=_themeFromIndex(prefs.getInt(_themeKey)??0);
     s._gauge=HudGaugeStyle.values[(prefs.getInt(_gaugeKey)??0).clamp(0,HudGaugeStyle.values.length-1).toInt()];
+    s._gtLayout=GtLayout.values[(prefs.getInt(_gtLayoutKey)??0).clamp(0,GtLayout.values.length-1).toInt()];
     s._mirror=prefs.getBool(_mirrorKey)??false;
     s._gear=(prefs.getInt(_gearKey)??0).clamp(0,6).toInt();
     s._unit=SpeedUnit.values[(prefs.getInt(_unitKey)??0).clamp(0,SpeedUnit.values.length-1).toInt()];
@@ -35,10 +37,11 @@ class AppSettings extends ChangeNotifier {
     return s;
   }
   static HudTheme _themeFromIndex(int i)=>HudTheme.all[i.clamp(0,HudTheme.all.length-1).toInt()];
-  HudTheme get theme=>_theme; HudGaugeStyle get gauge=>_gauge; bool get mirror=>_mirror; int get gear=>_gear; SpeedUnit get unit=>_unit; double get speedLimit=>_speedLimit; String get vehicleName=>_vehicleName; String get vehicleModel=>_vehicleModel; bool get obdEnabled=>_obdEnabled; bool get animations=>_animations; bool get showRpm=>_showRpm; bool get compact=>_compact;
+  HudTheme get theme=>_theme; HudGaugeStyle get gauge=>_gauge; GtLayout get gtLayout=>_gtLayout; bool get mirror=>_mirror; int get gear=>_gear; SpeedUnit get unit=>_unit; double get speedLimit=>_speedLimit; String get vehicleName=>_vehicleName; String get vehicleModel=>_vehicleModel; bool get obdEnabled=>_obdEnabled; bool get animations=>_animations; bool get showRpm=>_showRpm; bool get compact=>_compact;
   double toDisplaySpeed(double kmh)=>_unit==SpeedUnit.kmh?kmh:kmh*0.621371;
   Future<void> setTheme(HudTheme v)async{_theme=v;await _prefs.setInt(_themeKey,HudTheme.all.indexOf(v));notifyListeners();}
   Future<void> setGauge(HudGaugeStyle v)async{_gauge=v;await _prefs.setInt(_gaugeKey,v.index);notifyListeners();}
+  Future<void> setGtLayout(GtLayout v)async{_gtLayout=v;await _prefs.setInt(_gtLayoutKey,v.index);notifyListeners();}
   Future<void> setMirror(bool v)async{_mirror=v;await _prefs.setBool(_mirrorKey,v);notifyListeners();}
   Future<void> setGear(int v)async{_gear=v.clamp(0,6).toInt();await _prefs.setInt(_gearKey,_gear);notifyListeners();}
   Future<void> setUnit(SpeedUnit v)async{_unit=v;await _prefs.setInt(_unitKey,v.index);notifyListeners();}
