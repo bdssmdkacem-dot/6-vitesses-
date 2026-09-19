b3f16a9f0b8899c661357e76f5ee247e73a520d4
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-19 13:12:44 +0100

    feat(obd): add address connection to transport contract

--- lib/features/driving/obd/obd_transport.dart
+++ lib/features/driving/obd/obd_transport.dart
@@ -1,5 +1,6 @@
 abstract interface class ObdTransport {
   Future<void> connect();
+  Future<void> connectToAddress(String address);
   Future<void> disconnect();
   Future<String> send(String command);
   bool get connected;
