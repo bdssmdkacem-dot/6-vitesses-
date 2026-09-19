2ef43ae646e708d07a92380e27933c42782f028a
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-19 12:16:48 +0100

    fix(hud): tighten GT route map footprint

--- lib/features/hud/widgets/gt_layout_engine.dart
+++ lib/features/hud/widgets/gt_layout_engine.dart
@@ -27,8 +27,8 @@ class GtRouteMap extends StatelessWidget {
   final NavigationManeuver? nextManeuver; final bool compact;
   @override
   Widget build(BuildContext context)=>Container(
-    width: compact ? 210 : 230,
-    height: compact ? 100 : 108,
+    width: compact ? 190 : 200,
+    height: compact ? 90 : 92,
     padding: const EdgeInsets.all(8),
     decoration: BoxDecoration(
       color: Colors.black.withValues(alpha:.58),
