b8316f86ffd553ba460e5ec5f760c03fcdd564c2
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-19 15:08:28 +0100

    feat(phase-e): timestamp optional OBD telemetry

---
+++ obd_telemetry.dart
@@ -0,0 +1,23 @@
+enum ObdTelemetrySource { unavailable, gps, obd }
+
+class ObdTelemetry {
+  const ObdTelemetry({
+    required this.source,
+    this.engineLoad,
+    this.coolantTemperatureC,
+    this.rpm,
+    this.vehicleSpeedKmh,
+    this.throttlePosition,
+    this.timestamp,
+  });
+
+  final ObdTelemetrySource source;
+  final double? engineLoad;
+  final double? coolantTemperatureC;
+  final double? rpm;
+  final double? vehicleSpeedKmh;
+  final double? throttlePosition;
+  final DateTime? timestamp;
+
+  bool get available => source == ObdTelemetrySource.obd && vehicleSpeedKmh != null;
+}
