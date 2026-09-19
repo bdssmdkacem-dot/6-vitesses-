9b0ea77e8d3fd4a1254aba98ad2a1979dee6771d
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-19 13:12:36 +0100

    feat(obd): expose address-based adapter connection

--- lib/features/driving/obd/obd_adapter.dart
+++ lib/features/driving/obd/obd_adapter.dart
@@ -2,6 +2,7 @@ import 'obd_telemetry.dart';
 
 abstract interface class ObdAdapter {
   Future<void> connect();
+  Future<void> connectToAddress(String address);
   Future<void> disconnect();
   Stream<ObdTelemetry> get telemetry;
   bool get connected;
