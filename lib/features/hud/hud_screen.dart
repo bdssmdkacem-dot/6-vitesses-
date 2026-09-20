0120d01fcbe4661465bbc963815ef502115b2144
bdssmdkacem-dot <bdssmdkacem@gmail.com>
2026-09-20 04:10:27 +0100

    ui(gt): feed Touring trip telemetry

--- lib/features/hud/hud_screen.dart
+++ lib/features/hud/hud_screen.dart
@@ -331,6 +331,7 @@ class _HudScreenState extends State<HudScreen> with WidgetsBindingObserver {
                     animate: widget.settings.animations,
                     gtLayout: gtLayout,
                     gForce: _totalAccel / 9.80665,
+                    tripDistanceKm: _session.distanceKm,
                     longitudinalAccel: _longitudinalAccel,
                     gear: _estimatedGear(_speed),
                   ),
